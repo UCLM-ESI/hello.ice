9309e6a9c69470e2665698713b71c5d6522bac29
David Villa Alises <David.Villa@uclm.es>
2011-10-24 23:01:20 +0200

    atheist test for py-ami

--- py-ami/Client-end.py
+++ py-ami/Client-end.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"usage: {} <server> <value>"
 
 import sys, Ice
-Ice.loadSlice('ami.ice')
-import Demo
+Ice.loadSlice('../ami.ice')
+import UCLM
 
 
-class client (Ice.Application):
-
+class Client(Ice.Application):
     def run(self, argv):
         base = self.communicator().stringToProxy(argv[1])
 
-        prx = Demo.EmployeesPrx.checkedCast(base)
+        prx = UCLM.MathPrx.checkedCast(base)
 
         if not prx:
             raise RuntimeError("Invalid proxy")
 
-        async_result = prx.begin_getName(1)
+        async_result = prx.begin_factorial(argv[2])
         print 'this was a async call'
 
-        print prx.end_getName(async_result)
+        print prx.end_factorial(async_result)
 
         return 0
 
 
-sys.exit(client().main(sys.argv))
+if len(sys.argv) != 3:
+    print __doc__.format(__file__)
+    sys.exit(1)
+
+app = Client()
+sys.exit(app.main(sys.argv))
