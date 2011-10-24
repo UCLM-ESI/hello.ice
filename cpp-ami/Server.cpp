419e0bf7b297114bcf86fb3a1e879ca8e13d8934
David Villa Alises <David.Villa@uclm.es>
2011-10-24 23:18:35 +0200

    C++ AMI example

---
+++ cpp-ami/Server.cpp
@@ -0,0 +1,28 @@
+// -*- coding:utf-8; tab-width:4; mode:cpp -*-
+
+#include <Ice/Ice.h>
+#include <amiI.h>
+using namespace Ice;
+
+class Server: public Ice::Application {
+  int run(int argc, char* argv[]) {
+
+    ObjectAdapterPtr oa = communicator()->createObjectAdapter("OA");
+    ObjectPrx prx = oa->add(new UCLM::MathI(),
+							communicator()->stringToIdentity("hello1"));
+    oa->activate();
+
+    std::cout << communicator()->proxyToString(prx) << std::endl;
+
+    shutdownOnInterrupt();
+    communicator()->waitForShutdown();
+
+    return 0;
+  }
+};
+
+
+int main(int argc, char* argv[]) {
+  Ice::Application* app = new Server();
+  return app->main(argc, argv);
+}
