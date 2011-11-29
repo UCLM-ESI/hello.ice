f8b2734fa61d1c5c7802ae31e5ea3dedeb38d9d4
David Villa Alises <David.Villa@uclm.es>
2011-11-29 11:18:19 +0100

    some refactoring

--- java-freeze/Server.java
+++ java-freeze/Server.java
@@ -1,35 +1,33 @@
-
 public class Server extends Ice.Application {
     public int run (String[] args) {
-	shutdownOnInterrupt();
-
+				shutdownOnInterrupt();
         communicator().addObjectFactory(new HelloFactory(),
-					UCLM.HelloPersistent.ice_staticId());
+																				UCLM.HelloPersistent.ice_staticId());
 
-	Ice.ObjectAdapter adapter = communicator().createObjectAdapter("OA");
+				Ice.ObjectAdapter adapter = communicator().createObjectAdapter("OA");
         Freeze.Evictor evictor =
-	    Freeze.Util.createBackgroundSaveEvictor(adapter, "db", "hello",
-						    new HelloInitializer(),
-						    null, true);
-	for (int i=0; i<5; ++i) {
-            Ice.Identity identity = Ice.Util.stringToIdentity("hello" + i);
+						Freeze.Util.createBackgroundSaveEvictor(adapter, "db", "hello",
+																										new HelloInitializer(),
+																										null, true);
+				for (int i=0; i<5; ++i) {
+            Ice.Identity identity = Ice.Util.stringToIdentity("Hello" + i);
             if (!evictor.hasObject(identity)) {
                 Ice.ObjectPrx obj = evictor.add(new HelloI(), identity);
                 System.out.println(communicator().proxyToString(obj));
-	    }
-	}
+						}
+				}
 
-	adapter.addServantLocator(evictor, "");
-	adapter.activate();
-	System.out.println("Ready");
-	communicator().waitForShutdown();
+				adapter.addServantLocator(evictor, "");
+				adapter.activate();
+				System.out.println("Ready");
 
-	System.out.println("Done");
-	return 0;
+				communicator().waitForShutdown();
+				System.out.println("Done");
+				return 0;
     }
 
     public static void main (String[] args) {
-	Server app = new Server();
-	System.exit(app.main("Server", args));
+				Server app = new Server();
+				System.exit(app.main("Server", args));
     }
 }
