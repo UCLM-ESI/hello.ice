d5b7beca1c5e09acc52479c2be0c7441021c3747
Oscar Aceña <oscaracena@gmail.com>
2011-12-22 14:46:08 +0000

    Dynamic Ice with Python; basic example

---
+++ py-dynamic/example.ice
@@ -0,0 +1,7 @@
+// -*- mode: c++; coding: utf-8 -*-
+
+module Example {
+  interface Hello {
+    bool say(string message);
+  };
+};
---
+++ py-dynamic/standard-dispatching.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- mode: python; coding: utf-8 -*-
+
+import sys
+import Ice
+
+Ice.loadSlice("example.ice")
+import Example
+
+
+class HelloI(Example.Hello):
+
+    def say(self, message, current=None):
+        print "say:", message
+        return True
+
+
+class StandardDispatching(Ice.Application):
+
+    def run(self, args):
+        self.ic = self.communicator()
+
+        self.create_adapter()
+        self.append_servant()
+        self.wait_events()
+
+    def create_adapter(self):
+        self.oa = self.ic.createObjectAdapterWithEndpoints(
+            "Adapter", "tcp -p 1234")
+        self.oa.activate()
+
+    def append_servant(self):
+        srv = HelloI()
+        oid = self.ic.stringToIdentity("StandardDispatching")
+        prx = self.oa.add(srv, oid)
+
+        print "Use proxy: '{0}'".format(prx)
+
+    def wait_events(self):
+        self.shutdownOnInterrupt()
+        self.ic.waitForShutdown()
+
+
+StandardDispatching().main(sys.argv)
+
