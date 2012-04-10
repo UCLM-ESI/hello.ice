f3acd2ed39101d4173119f5f0127bfa85f6f4d4d
David Villa Alises <David.Villa@gmail.com>
2012-04-10 19:54:05 +0200

    Java AMI Client callback

--- java-ami/Client_end.java
+++ java-ami/Client_end.java
@@ -1,18 +1,18 @@
 public class Client_end extends Ice.Application {
-  public int run(String[] args) {
-    Ice.ObjectPrx proxy = communicator().stringToProxy(args[0]);
-    Example.MathPrx math = Example.MathPrxHelper.checkedCast(proxy);
+    public int run(String[] args) {
+	Ice.ObjectPrx proxy = communicator().stringToProxy(args[0]);
+	Example.MathPrx math = Example.MathPrxHelper.checkedCast(proxy);
 
-    Ice.AsyncResult async_result = math.begin_factorial(Integer.parseInt(args[1]));
-    System.out.println("this was an async call");
+	Ice.AsyncResult async_result = math.begin_factorial(Integer.parseInt(args[1]));
+	System.out.println("this was an async call");
 
-    System.out.println(math.end_factorial(async_result));
+	System.out.println(math.end_factorial(async_result));
 
-    return 0;
-  }
+	return 0;
+    }
 
-  static public void main(String[] args) {
-    Client_end app = new Client_end();
-    app.main("Client", args);
-  }
+    static public void main(String[] args) {
+	Client_end app = new Client_end();
+	app.main("Client", args);
+    }
 }
