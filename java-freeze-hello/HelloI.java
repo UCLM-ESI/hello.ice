87e9ba50f9b551c4a2f8aaf88cb398440039bc82
David Villa Alises <David.Villa@gmail.com>
2012-04-12 23:28:45 +0200

    freeze hello samples removed

--- java-freeze-hello/HelloI.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloI extends UCLM.HelloPersistent {
-    public void puts(String str, Ice.Current current) {
-	System.out.println("Hello (i=" + useCount + "): " + str);
-        useCount += 1;
-    }
-}
