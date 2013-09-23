ec6f57a76e200fa199055c025757e7bcf5b6644c
David Villa Alises <David.Villa@gmail.com>
2013-09-23 16:31:20 +0200

    controlling a desktop missile turret

---
+++ py-turret-gtk/Server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+
+import sys
+import Ice
+
+Ice.loadSlice('Armory.ice')
+import Armory
+
+sys.path.append('/home/david/repos/librocket')
+
+import turret
+
+
+class PanTiltI(Armory.PanTilt):
+    def __init__(self):
+        self.driver = turret.Turret()
+
+    def down(self, current=None):
+        self.driver.down()
+
+    def up(self, current=None):
+        self.driver.up()
+
+    def left(self, current=None):
+        self.driver.left()
+
+    def right(self, current=None):
+        self.driver.right()
+
+    def stop(self, current=None):
+        self.driver.stop()
+
+
+class Server(Ice.Application):
+    def run(self, argv):
+        broker = self.communicator()
+        servant = PanTiltI()
+
+        adapter = broker.createObjectAdapter("TurretAdapter")
+        proxy = adapter.add(servant, broker.stringToIdentity("turret1"))
+
+        print(proxy)
+
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        broker.waitForShutdown()
+
+        return 0
+
+
+server = Server()
+sys.exit(server.main(sys.argv))
