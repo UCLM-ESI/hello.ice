a9a8c58ece960472558b0fa6b7bad1059fc7c236
David Villa Alises <David.Villa@gmail.com>
2013-10-20 13:57:09 +0200

    query client
    --HG--
    rename : py/Client-query.py => icegrid/query-client.py

--- icegrid/query-client.py
+++ icegrid/query-client.py
@@ -16,11 +16,17 @@ class client(Ice.Application):
 
     def run(self, argv):
         query = self.get_query()
-
         if not query:
             raise RuntimeError('Invalid proxy')
 
+        # get object by identity
+        proxy = query.findObjectById(self.communicator().stringToIdentity('printer1'))
+        # or by type
         proxy = query.findObjectByType('::Example::Printer')
+
+        if not proxy:
+            raise RuntimeError('Object not found')
+
         printer = Example.PrinterPrx.checkedCast(proxy)
         printer.write('Hello World!')
 
