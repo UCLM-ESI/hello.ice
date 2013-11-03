323d25559a60c1d37904e072c3640069c25b7c0a
David Villa Alises <David.Villa@gmail.com>
2013-11-03 20:29:47 +0100

    new work_queue

--- py-amd/work_queue.py
+++ py-amd/work_queue.py
@@ -1,6 +1,7 @@
 # -*- mode:python; coding:utf-8; tab-width:4 -*-
 
 from threading import Thread
+from Queue import Queue
 
 
 def factorial(n):
@@ -10,22 +11,36 @@ def factorial(n):
     return n * factorial(n - 1)
 
 
-class Worker(Thread):
+class WorkQueue(Thread):
     QUIT = 'QUIT'
+    SKIP = 'SKIP'
 
-    def __init__(self, queue):
-        super(Worker, self).__init__()
-        self.queue = queue
+    def __init__(self):
+        super(WorkQueue, self).__init__()
+        self.queue = Queue()
 
     def run(self):
         while True:
             job = self.queue.get()
-            if job == Worker.QUIT:
+            if job == self.QUIT:
                 self.queue.task_done()
                 break
 
+            if job == self.SKIP:
+                self.queue.put(self.QUIT)
+                Job.execute = Job.cancel
+                self.queue.task_done()
+                continue
+
             job.execute()
             self.queue.task_done()
+
+    def add(self, cb, value):
+        self.queue.put(Job(cb, value))
+
+    def destroy(self):
+        self.queue.put(self.SKIP)
+        self.queue.join()
 
 
 class Job(object):
@@ -35,3 +50,6 @@ class Job(object):
 
     def execute(self):
         self.cb.ice_response(factorial(self.value))
+
+    def cancel(self):
+        self.cb.ice_exception(Example.RequestCancelException())
