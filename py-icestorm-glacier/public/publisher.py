4bb85823e527183f6788e9ac724e963776c734a6
Oscar Aceña <oscaracena@gmail.com>
2017-06-30 13:10:24 +0200

    python, IS and G2: basic IS setup with publisher and subscriber, not using Glacier yet

---
+++ py-icestorm-glacier/public/publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3 -u
+# -*- mode: python; coding: utf-8 -*-
+
+import sys
+import Ice
+import IceStorm
+
+Ice.loadSlice('Printer.ice')
+import Example  # noqa
+
+
+class Publisher(Ice.Application):
+    def run(self, args):
+        printer = self.get_publisher("PrinterTopic")
+
+        print("publishing 10 'Hello World' events")
+        for i in range(10):
+            printer.write("Hello World %s!" % i)
+
+    def get_publisher(self, topic):
+        topic = self.get_topic(topic)
+        pub = topic.getPublisher()
+        return Example.PrinterPrx.uncheckedCast(pub)
+
+    def get_topic(self, topic_name):
+        ic = self.communicator()
+        mgr = ic.propertyToProxy("IceStorm.TopicManager.Proxy")
+        mgr = IceStorm.TopicManagerPrx.checkedCast(mgr)
+
+        try:
+            return mgr.retrieve(topic_name)
+        except IceStorm.NoSuchTopic:
+            return mgr.create(topic_name)
+
+
+if __name__ == "__main__":
+    Publisher().main(sys.argv)
