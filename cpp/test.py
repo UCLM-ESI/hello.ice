710e1d42fff052b5da7bbd8aa1a52a0ed42659b2
David Villa Alises <David.Villa@gmail.com>
2018-09-25 22:27:59 +0200

    migrating tests to prego
    --HG--
    rename : test/Dockerfile => _test/Dockerfile
    rename : test/test-sample.py => _test/test-sample.py

--- cpp/test.py
+++ cpp/test.py
@@ -1,19 +1,9 @@
 #!/usr/bin/prego
 # -*- mode:python; coding:utf-8 -*-
 
-from hamcrest import contains_string
-from prego import TestCase, Task, running
+from _test import ClientServerMixin
 
 
-class Hello(TestCase):
+class Hello(ClientServerMixin):
     def test_client_server(self):
-        servertask = Task('server', detach=True)
-        server = servertask.command('./Server --Ice.Config=Server.config',
-                                    cwd='$testdir', signal=2)
-        servertask.assert_that(server.stdout.content, contains_string('Hello World!'))
-
-        clientside = Task('client')
-        clientside.wait_that(server, running())
-        clientside.wait_that(server.stdout.content, contains_string('printer1'))
-        clientside.command('./Client "$(head -1 %s)"' % server.stdout.path,
-                           cwd='$testdir')
+        self.make_client_server('./Client', './Server')
