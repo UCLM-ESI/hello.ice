4c118070e7e9a3c7b1743f7c7e8fc2d00804e3e5
David Villa Alises <David.Villa@gmail.com>
2019-05-17 13:13:46 +0200

    retry_cast method

--- icegrid-factory/client.py
+++ icegrid-factory/client.py
@@ -3,6 +3,7 @@
 
 import sys
 import Ice
+import time
 
 Ice.loadSlice('-I. --all factory.ice')
 Ice.loadSlice('-I. --all printer.ice')
@@ -14,17 +15,28 @@ import Example
 class Client(Ice.Application):
     def run(self, argv):
         proxy = self.communicator().stringToProxy(argv[1])
-        factory = IceCloud.ServerFactoryPrx.checkedCast(proxy)
-
-        if not factory:
-            raise RuntimeError('Invalid proxy')
+        factory = self.retry_cast(IceCloud.ServerFactoryPrx, proxy)
 
         proxy = factory.make('node1', 'PrinterTemplate', {'name': 'printer1'})
-        printer = Example.PrinterPrx.checkedCast(proxy)
+        printer = self.retry_cast(Example.PrinterPrx, proxy)
 
         printer.write('Hello World!')
+        print('ok')
 
-        return 0
+    def retry_cast(self, cast, proxy):
+        for i in range(5):
+            try:
+                retval = cast.checkedCast(proxy)
+                break
+            except Ice.ObjectNotExistException:
+                print('.', end='')
+                time.sleep(0.5)
+            else:
+                raise Exception("Object '{}' not available".format(proxy))
 
+        if not retval:
+            raise RuntimeError("Invalid proxy: '{}'".format(proxy))
+
+        return retval
 
 sys.exit(Client().main(sys.argv))
