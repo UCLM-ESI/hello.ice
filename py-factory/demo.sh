dc130be5ef57b270121e76c6469641a2a646be22
David Villa Alises <david.villa@gmail.com>
2025-11-01 16:51:57 +0100

    fix tmux demo

--- py-factory/demo.sh
+++ py-factory/demo.sh
@@ -2,6 +2,6 @@
 
 tmux -f .tmux.conf new-session -d -s session
 tmux -f .tmux.conf split-window -h
-tmux -f .tmux.conf send-keys -t session:0.0 "./server.py --Ice.Config=server.config | tee proxy.out" C-m
-tmux -f .tmux.conf send-keys -t session:0.1 "sleep 1;./client.py '$(cat proxy.out)'" C-m
+tmux -f .tmux.conf send-keys -t session:0.0 "make run-server" C-m
+tmux -f .tmux.conf send-keys -t session:0.1 "sleep 1;make run-client" C-m
 tmux -f .tmux.conf attach-session -t session
