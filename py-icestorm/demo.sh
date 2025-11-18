58b6aec9e96e7a5632b80607f42027fe24a2487b
David Villa Alises <david.villa@gmail.com>
2025-11-18 12:32:24 +0100

    avoid tmux.conf

--- py-icestorm/demo.sh
+++ py-icestorm/demo.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-tmux -f .tmux.conf new-session -d -s session
-tmux -f .tmux.conf split-window -h
-tmux -f .tmux.conf split-window -v
-tmux -f .tmux.conf send-keys -t session:0.0 "make start && make run-publisher" C-m
-tmux -f .tmux.conf send-keys -t session:0.1 "make run-subscriber" C-m
-tmux -f .tmux.conf send-keys -t session:0.2 "make run-subscriber" C-m
-tmux -f .tmux.conf attach-session -t session
+tmux new-session -d -s session
+
+tmux set-option -g mouse on
+tmux setw -g monitor-activity on
+tmux set-option -g visual-activity on
+tmux bind-key x kill-session
+tmux set-option -g set-titles off
+
+tmux split-window -h
+tmux split-window -v
+
+tmux send-keys -t session:0.0 "make start && make run-publisher" C-m
+tmux send-keys -t session:0.1 "make run-subscriber" C-m
+tmux send-keys -t session:0.2 "make run-subscriber" C-m
+
+tmux attach-session -t session
