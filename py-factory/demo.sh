fc14d2036eb3ca7ea775467ef2952f6c58db6840
David Villa Alises <david.villa@gmail.com>
2025-11-18 12:36:28 +0100

    rm tmux.conf

--- py-factory/demo.sh
+++ py-factory/demo.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-tmux -f .tmux.conf new-session -d -s session
-tmux -f .tmux.conf split-window -h
-tmux -f .tmux.conf send-keys -t session:0.0 "make run-server" C-m
-tmux -f .tmux.conf send-keys -t session:0.1 "sleep 1;make run-client" C-m
-tmux -f .tmux.conf attach-session -t session
+tmux new-session -d -s session
+
+tmux set-option -g mouse on
+tmux setw -g monitor-activity on
+tmux set-option -g visual-activity on
+tmux bind-key x kill-session
+tmux set-option -g set-titles off
+
+tmux split-window -h
+tmux send-keys -t session:0.0 "make run-server" C-m
+tmux send-keys -t session:0.1 "sleep 1;make run-client" C-m
+
+tmux attach-session -t session
